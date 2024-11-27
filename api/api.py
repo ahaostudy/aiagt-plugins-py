1433724335aaf9413195293dcb8d6d80cafb8a37
ahaostudy <1993584108@qq.com>
2024-11-27 20:17:02 +0800

    fix: package path error

--- api/api.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-import google_search
-
-api = Blueprint('api_v1', __name__)
-api.register_blueprint(google_search.api, url_prefix='/google_search')
--- api/index.py
+++ api/index.py
@@ -1,17 +1,21 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from dotenv import load_dotenv
-from api import api
+
+from api import google_search
 
 load_dotenv()
 app = Flask(__name__)
-
-app.register_blueprint(api, url_prefix='/api/v1')
 
 
 @app.route('/')
 def home():
     return 'Aiagt plugin apis'
 
+
+v1 = Blueprint('api_v1', __name__)
+v1.register_blueprint(google_search.api, url_prefix='/google_search')
+
+app.register_blueprint(v1, url_prefix='/api/v1')
 
 if __name__ == '__main__':
     app.run(debug=True)
